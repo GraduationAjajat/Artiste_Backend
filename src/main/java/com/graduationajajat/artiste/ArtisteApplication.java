3c3816ea1e3cd0c4b876ec5778b7662de45467fc
신혜승 <hyeseung2000@gmail.com>
2022-07-04 16:37:21 +0900

    Initial commit

---
+++ src/main/java/com/graduationajajat/artiste/ArtisteApplication.java
@@ -0,0 +1,13 @@
+package com.graduationajajat.artiste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtisteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtisteApplication.class, args);
+	}
+
+}
